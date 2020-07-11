ed0d545bcda2f9f9fe0d2b79a57de47435280837
ericgaona <ericgaona0413@gmail.com>
2020-07-11 23:25:40 +0000

    prueba_2 - it is working

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -4,13 +4,19 @@ function sendMail(contactForm) {
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
     })
-        .then(
-            function (response) {
-                console.log("SUCCESS", response);
-            },
-            function (error) {
-                console.log("FAILED", error);
-            }
-        );
+    .then(
+        function (response) {
+            console.log("SUCCESS", response);  
+            document.getElementById("success").style.display = "block" ;           
+            document.getElementById("success").innerHTML = "<strong>Thanks. Your email has been sent successfully.</strong>";
+
+        },
+        function (error) {
+            console.log("FAILED", error);
+            document.getElementById("success").style.display = "block" ;
+            document.getElementById("fail").innerHTML = "<strong>Sorry. There is an issue, try it again.</strong>";
+
+        }
+    );
     return false;  // To block from loading a new page
 }
